5104d039552a20b11a2d151d603dae75835e7926
Faathir <64758987+AudiFaathirmansyahMashar@users.noreply.github.com>
2020-07-03 22:54:56 +0800

    Update Values.kt

--- myapplication/src/main/java/id/sharekom/myapplication/helpers/Values.kt
+++ myapplication/src/main/java/id/sharekom/myapplication/helpers/Values.kt
@@ -3,5 +3,5 @@ package id.sharekom.myapplication.helpers
 object Values {
     const val DATA_MAIN = "datamain"
     const val MAIN_URL = "https://api.github.com/"
-    const val TOKEN = "de820dac6a73ae85c84b295420593a9f3f677f6c"
+    const val TOKEN = "INPUT YOUR TOKEN HERE"
 }
